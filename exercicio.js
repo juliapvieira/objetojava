d8fb6f8dc6902ad14a50889110d5b7616efd8ca1
julia <julia.pardim@icloud.com>
2026-09-25 10:21:04 -0300

    exercício

---
+++ exercicio.js
@@ -0,0 +1,140 @@
+//Exercício 1 – Sistema de Login
+//Crie um objeto usuario com:
+//login
+//senha
+//tipo
+//Depois, exiba a mensagem:
+//O login do administrador é admin e a senha é 1234, muito fraca por sinal.
+
+let usuario = {
+    login: "admin",
+    senha: "1234",
+    tipo: "administrador"
+};
+console.log("O login do " + usuario.tipo + " é " + usuario.login + 
+" e a senha é " + usuario.senha + ", muito fraca por sinal.");
+//________________________________________________________________________________________
+//Exercício 2 – Cadastro de Aluno
+//Crie um objeto aluno com:
+//nome
+//curso
+//turma
+//Depois, exiba a mensagem:
+//O aluno Lucas está matriculado no curso de Desenvolvimento de Sistemas, na turma 2º DS.
+let aluno = {
+    nome: "Lucas",
+    curso: "Desenvolviento de sistemas",
+    turma: "2 DS",
+};
+console.log("O aluno " + aluno.nome + " está matriculado no curso de " + aluno.curso + " na turma " + aluno.turma )
+
+//Exercício 3 – Professor
+//Crie um objeto professor com:
+//nome
+//disciplina
+//turno
+//Depois, exiba a mensagem:
+//O professor Carlos ministra a disciplina de JavaScript no turno da manhã.
+
+let professor = {
+    nome: "Carlos",
+    disciplina: "JavaScript",
+    turno: "manhã",
+};
+console.log("O professor " + professor.nome + " ministra a disciplina de " + professor.disciplina + " no turno da " + professor.turno)
+
+
+//Exercício 4 – Empresa
+//Crie um objeto empresa com:
+//nome
+//area
+//cidade
+//Depois, exiba a mensagem:
+//A empresa Tech Solutions atua na área de Tecnologia e fica em Fortaleza.
+let empresa = {
+    nome: "Tech Solutions",
+    area: "Tecnologia",
+    cidade: "Fortaleza"
+};
+console.log("A empresa " + empresa.nome + " atua na área de " + empresa.area + 
+" e fica em " + empresa.cidade);
+//____________________________________________________________________________________________________________________________________
+
+//Exercício 5 – Computador
+//Crie um objeto computador com:
+//marca
+//processador
+//memoria
+//Depois, exiba a mensagem:
+//O computador da marca Dell possui processador Intel i5 e memória de 8GB.
+let computador = {
+    marca: "Dell",
+    processador: "Intel i5",
+    memoria: "8GB",
+};
+
+console.log("O computador da marca " + computador.marca + " possui o processador " + computador.processador + " e memória de " + computador.memoria)
+//______________________________________________________________________________________________________________________________________
+
+//Exercício 6 – Livro
+//Crie um objeto livro com:
+//titulo
+//autor
+//paginas
+//Depois, exiba a mensagem:
+//O livro JavaScript Básico foi escrito por Ana Silva e possui 200 páginas.
+
+let livro = {
+    titulo: "JavaScript",
+    autor: "Ana Silva",
+    paginas:"200 páginas",
+};
+console.log("O livro " + livro.titulo + " foi escrito por " + livro.autor + " e possui " + livro.paginas )
+//________________________________________________________________________________________________________________________________________
+
+//Exercício 7 – Filme
+//Crie um objeto filme com:
+//nome
+//genero
+//ano
+//Depois, exiba a mensagem:
+//O filme Matrix é do gênero Ficção e foi lançado em 1999.
+let filme = {
+    nome: "Matrix",
+    genero:"Ficção",
+    ano:"1999",
+
+};
+console.log("O filme " + filme.nome + " é do gênero " + filme.genero + " e foi lançado em " + filme.ano)
+//_______________________________________________________________________________________________________________________________________
+
+//Exercício 8 – Jogo
+//Crie um objeto jogo com:
+//nome
+//plataforma
+//genero
+//Depois, exiba a mensagem:
+//O jogo Minecraft roda na plataforma PC e pertence ao gênero Sandbox.
+let jogo = {
+    nome: "Minecraft",
+    plataforma:"PC",
+    genero:"Sandbox"
+};
+console.log("O jogo " + jogo.nome + " roda na plataforma " + jogo.plataforma + " e pertence ao gênero " + jogo.genero )
+//_______________________________________________________________________________________________________________________________________
+
+//Exercício 9 – Conta de Usuário
+//Crie um objeto conta com:
+//usuario
+//senha
+//nivel
+//Depois, exiba a mensagem:
+//O usuário eduardo possui nível administrador e utiliza a senha 1234, muito fraca por sinal.
+
+//Exercício 10 – Escola
+//Crie um objeto escola com:
+//nome
+//cidade
+//turno
+//Depois, exiba a mensagem:
+//A escola EEEP funciona na cidade de Fortaleza no turno da manhã.
